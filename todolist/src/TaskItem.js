bd974aed6e1c5009224d73a383584a3683debf03
nerea <nerea.fernandez@enti.cat>
2023-02-24 11:37:20 +0100

    Empezamos con Todo List

---
+++ todolist/src/TaskItem.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class TaskItem extends React.Component{
+
+	render(){
+	  return(
+		<li>Estudiar<button type="button">❌</button></li>
+	
+	  );
+	}
+
+}
+
+export default TaskItem;
